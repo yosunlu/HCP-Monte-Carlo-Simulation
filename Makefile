d271f9f4a033a4feab496a4d162089c2db47da3c
yushanlu <yushanlu1031@gmail.com>
2024-11-13 22:13:34 -0600

    update script

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
+push:
+	git add .
+	git commit -m "update script"
+	git push
+
 cuda:
 	chmod +x cuda.sh
 	./cuda.sh
--- cuda.sh
+++ cuda.sh
@@ -7,7 +7,10 @@
 #SBATCH --error="./output/cuda.err"
 #SBATCH --gres=gpu:1
 
+mkdir -p output
+
 module load nvidia/cuda/11.8.0
 module load gcc/9.4.0
-nvcc -o barrier_option_pricing main.cpp kernel.cu -lcurand -Xcompiler /openmp
+
+nvcc -o barrier_option_pricing main.cpp kernel.cu -lcurand -Xcompiler
 ./kernael.cu >> ./output/cuda.out
