6cb6d0d7b83377611c30b41ffce4dbb210196106
yushanlu <yushanlu1031@gmail.com>
2024-11-27 15:44:35 -0600

    Test: Shared Mem

--- scripts/cuda.sh
+++ scripts/cuda.sh
@@ -15,5 +15,6 @@ module load nvidia/cuda/11.8.0
 module load gcc/9.4.0
 nvidia-smi
 
+
 nvcc -o barrier_option_pricing ./cuda/cuda.cpp ./cuda/kernel.cu -lcurand -Xcompiler -fopenmp
 ./barrier_option_pricing 
