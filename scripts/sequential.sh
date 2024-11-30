21a8c1e6da153f3fd1154a3a126be7e202c99b0b
yushanlu <yushanlu1031@gmail.com>
2024-11-30 16:37:23 -0600

    Test Sequential

--- scripts/sequential.sh
+++ scripts/sequential.sh
@@ -14,5 +14,19 @@ mkdir -p output
 module load nvidia/cuda/11.8.0
 module load gcc/9.4.0
 
+# Print CPU information
+echo "****************** CPU INFO ******************"
+lscpu
+
+# Print GPU information
+echo "****************** GPU INFO ******************"
+nvidia-smi
+
+# Compile the program
 nvcc -o barrier_option_pricing_seq ./sequential/sequential.cpp -lcurand -Xcompiler -fopenmp
+
+# Run the program
+echo "****************** PROGRAM OUTPUT ******************"
 ./barrier_option_pricing_seq
+
+rm barrier_option_pricing_seq
